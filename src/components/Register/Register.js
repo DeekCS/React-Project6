a0efcd9aee09df48f1c9b5a0055e3a4cb9877594
DeekCS <31720001079@std.bau.edu.jo>
2022-01-01 23:34:56 +0200

    initalize project

---
+++ src/components/Register/Register.js
@@ -0,0 +1,12 @@
+import React, {useEffect,useState} from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Register</h1>
+        </div>
+    );
+
+}
+
+export default Register;
