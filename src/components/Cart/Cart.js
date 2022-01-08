92836bf99ae1225d8dee4450321517a3bd16cecb
DeekCS <31720001079@std.bau.edu.jo>
2022-01-08 18:14:07 +0200

    change cart style

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useCart } from 'react-use-cart';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import { Input, Tooltip } from '@mui/material';
 import swal from 'sweetalert';
-import CartStyle from '../Header/Cart.style';
-import { Link } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
+import './cart.scss';
+import Button from '@mui/material/Button';
 const Cart = () => {
+  const navigate = useNavigate();
   let {
     isEmpty,
     totalUniqueItems,
@@ -31,14 +23,15 @@ const Cart = () => {
   const [discountClicked, setDiscountClicked] = useState(false);
   const handleCoupon = (e) => {
     e.preventDefault();
-
+    console.log(coupon);
     if (coupon === '') {
       swal('Oops!', 'Please enter a coupon code', 'error').then((_) => {
         setCoupon('');
       });
       return false;
     }
-    if (coupon.toLowerCase() === 'OCA-20') {
+    if (coupon.toLowerCase() === 'oca-22') {
+      console.log('coupon applied');
       setDiscount(0.1);
       setDiscountedTotal(cartTotal * 0.9);
       setSubTotal(cartTotal * 0.9);
@@ -82,45 +75,6 @@ const Cart = () => {
     }
   }, [discountClicked, cartTotal]);
 
-  const handleDelete = (id) => {
-    swal({
-      title: 'Are you sure?',
-      text: 'Once deleted, you will not be able to recover this imaginary file!',
-      icon: 'warning',
-      buttons: true,
-      dangerMode: true,
-    }).then((r) => {
-      if (r) {
-        removeItem(id);
-        swal('Poof! Your imaginary file has been deleted!', {
-          icon: 'success',
-        }).then(
-          () =>
-            localStorage.getItem('react-use-cart') &&
-            localStorage.setItem(
-              'react-use-cart',
-              JSON.stringify({
-                ...JSON.parse(localStorage.getItem('react-use-cart')),
-                cartTotal: cartTotal - items[id].price,
-              })
-            )
-        );
-      } else {
-        swal('Your imaginary file is safe!').then(
-          () =>
-            localStorage.getItem('react-use-cart') &&
-            localStorage.setItem(
-              'react-use-cart',
-              JSON.stringify({
-                ...JSON.parse(localStorage.getItem('react-use-cart')),
-                cartTotal: cartTotal,
-              })
-            )
-        );
-      }
-    });
-  };
-
   const handleClearCart = (products) => {
     swal({
       title: 'Are you sure?',
@@ -161,95 +115,150 @@ const Cart = () => {
   };
 
   return (
-    <div className={'cart-container'} style={CartStyle.div}>
-      <div className="cart-title">
-        <h2>Cart</h2>
-      </div>
-      {isEmpty ? (
-        <div className="cart-empty">
-          <h3>Your cart is empty</h3>
-        </div>
-      ) : (
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table" style={CartStyle.table}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Product</TableCell>
-                <TableCell align="right">Price</TableCell>
-                <TableCell align="right">Quantity</TableCell>
-                <TableCell align="right">Total</TableCell>
-                <TableCell align="right">Remove</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {items.map((item) => (
-                <TableRow key={item.id}>
-                  <TableCell component="th" scope="row">
-                    {item.name}
-                  </TableCell>
-                  <TableCell align="right">{item.price}</TableCell>
-                  <TableCell align="right">
-                    <Input
-                      type="number"
-                      value={item.quantity}
-                      onChange={(e) =>
-                        updateItemQuantity(item.id, e.target.value)
-                      }
-                    />
-                  </TableCell>
-                  <TableCell align="right">
-                    {item.quantity * item.price}
-                  </TableCell>
-                  <TableCell align="right">
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={() => handleDelete(item.id)}
-                    >
-                      Remove
-                    </Button>
-                  </TableCell>
-                </TableRow>
+    <>
+      <div className="wrap">
+        <header className="cart-header cf">
+          <strong>{totalUniqueItems} Item(s) in Your Cart</strong>
+          {isEmpty ? '' : <span className="btn">Checkout</span>}
+        </header>
+        <div className="cart-table">
+          {isEmpty ? (
+            <div className="cart-empty">
+              <h2>Your cart is empty</h2>
+              <p>
+                You have no items in your shopping cart.
+                <br />
+                <Button onClick={() => navigate('/shop')}>
+                  Continue Shopping
+                </Button>
+              </p>
+            </div>
+          ) : (
+            <ul>
+              {items.map((item, index) => (
+                <li className="item" key={index}>
+                  <div className="item-main cf">
+                    <div className="item-block ib-info cf">
+                      <img
+                        className="product-img"
+                        src={item.image}
+                        alt="Product"
+                      />
+                      <div className="ib-info-meta">
+                        <span className="title">{item.name}</span>
+                        <span className="itemno">#{item.id}</span>
+                      </div>
+                    </div>
+                    <div className="item-block ib-qty">
+                      <input
+                        type="number"
+                        value={item.quantity}
+                        onChange={(e) =>
+                          updateItemQuantity(item.id, e.target.value)
+                        }
+                        className="qty"
+                      />
+                      <span className="price">
+                        <span>x</span> ${item.price}
+                      </span>
+                    </div>
+                    <div className="item-block ib-total-price">
+                      <span className="tp-price">
+                        ${item.quantity * item.price}
+                      </span>
+                      <span className="tp-remove">
+                        <i className="i-cancel-circle"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="item-foot cf">
+                    <div className="if-left">
+                      <span className="if-status">In Stock</span>
+                    </div>
+                    <div className="if-right"></div>
+                  </div>
+                </li>
               ))}
-            </TableBody>
-          </Table>
-          <Button>
-            <Link to="/checkout">Checkout</Link>
-          </Button>
-          <Button onClick={() => handleClearCart(items)}>Clear Cart </Button>
-        </TableContainer>
-      )}
-      {isEmpty ? (
-        ''
-      ) : (
-        <div>
-          <p>
-            {totalUniqueItems} item(s) - ${cartTotal}
-          </p>
-          <form onSubmit={handleCoupon}>
-            <input
-              type="text"
-              value={coupon}
-              placeholder="Enter a coupon code"
-              onChange={(e) => setCoupon(e.target.value)}
-            />
-            <button type="submit">Apply</button>
-          </form>
-          Total: ${cartTotal}
-          <br />
-          {discountClicked ? (
-            <p>
-              You Save: ${cartTotal - discountedTotal}
-              <br />
-              Total After Discount: ${subTotal}
-            </p>
-          ) : null}
-          <br />
+            </ul>
+          )}
         </div>
-      )}
-      <Link to="/shop">Continue Shopping</Link>
-    </div>
+
+        {isEmpty ? (
+          ''
+        ) : (
+          <div className="sub-table cf">
+            <div className="summary-block">
+              <div className="sb-promo">
+                <input
+                  type="text"
+                  value={coupon}
+                  placeholder="Enter a coupon code"
+                  onChange={(e) => setCoupon(e.target.value)}
+                />
+                <span className="btn" onClick={handleCoupon}>
+                  Apply
+                </span>
+              </div>
+              <ul>
+                <li className="subtotal">
+                  <span className="sb-label">Total</span>
+                  <span className="sb-value"> ${cartTotal}</span>
+                </li>
+                {discountClicked ? (
+                  <>
+                    <li className="shipping">
+                      <span className="sb-label">You Save </span>
+                      <span className="sb-value">
+                        ${cartTotal - discountedTotal}
+                      </span>
+                    </li>
+                    <li className="tax">
+                      <span className="sb-label">Total After Discount </span>
+                      <span className="sb-value">${subTotal}</span>
+                    </li>
+                    <li className="tax-calculate">
+                      <input type="text" value="06484" className="tax" />
+                      <span className="btn">Calculate</span>
+                    </li>
+                    <li className="grand-total">
+                      <span className="sb-label">Total</span>
+                      <span className="sb-value">${subTotal}</span>
+                    </li>
+                  </>
+                ) : null}
+              </ul>
+            </div>
+            <div className="copy-block">
+              <p>
+                Items will be saved in your cart for 30 days. To save items
+                longer, add them to your <a href="#">Wish List</a>.
+              </p>
+              <p className="customer-care">
+                Call us M-F 8:00 am to 6:00 pm EST
+                <br />
+                (877)-555-5555 or <a href="#">contact us</a>. <br />
+              </p>
+            </div>
+          </div>
+        )}
+        {isEmpty ? (
+          ''
+        ) : (
+          <div className="cart-footer cf">
+            <span className="btn" onClick={handleClearCart}>
+              Clear Cart
+            </span>
+            <span className="cont-shopping" onClick={() => navigate('/shop')}>
+              <i className="i-angle-left"></i>Continue Shopping
+            </span>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
 export default Cart;
+/*
+
+ */
